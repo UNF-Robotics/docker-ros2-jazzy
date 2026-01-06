87756f092c31d59c492df84a213327fe41beaf8b
William L. Thomson Jr. <w@wltjr.com>
2026-01-05 19:49:03 -0500

    commands.sh: Drop && split into individual commands

--- commands.sh
+++ commands.sh
@@ -9,13 +9,14 @@ echo "[[ -d /opt/ros_ws/install ]] && source /opt/ros_ws/install/setup.sh" \
 mkdir -p /opt/ros_ws/src
 
 # ros2_laser_scan_merger
-cd /opt/ros_ws/src && \
-	wget https://github.com/mich1342/ros2_laser_scan_merger/archive/refs/heads/main.zip && \
-	unzip main.zip && rm main.zip && \
-	mv ros2_laser_scan_merger-main ros2_laser_scan_merger && \
-	cd ../ && \
-	. /opt/ros/jazzy/setup.sh && \
-	colcon build --symlink-install --packages-select ros2_laser_scan_merger
+cd /opt/ros_ws/src
+wget https://github.com/mich1342/ros2_laser_scan_merger/archive/refs/heads/main.zip
+unzip main.zip
+rm main.zip
+mv ros2_laser_scan_merger-main ros2_laser_scan_merger
+cd ../
+. /opt/ros/jazzy/setup.sh
+colcon build --symlink-install --packages-select ros2_laser_scan_merger
 
 # common commands added to history
 echo "ros2 teleop_twist_keyboard teleop_twist_keyboard" \
